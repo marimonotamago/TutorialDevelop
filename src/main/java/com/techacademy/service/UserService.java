befea5b3c9e4dffb091c6ef831319426fc0718ce
marimonotamago <golupio24@gmail.com>
2024-09-13 12:58:09 +0900

    chapter8までのコミット

--- src/main/java/com/techacademy/service/UserService.java
+++ src/main/java/com/techacademy/service/UserService.java
@@ -21,11 +21,18 @@ public class UserService {
         // リポジトリのfindAllメソッドを呼び出す <クラス名>はアロー括弧list型にするため
         return userRepository.findAll();
     }
-    // ----- 追加:ここから -----
+
+ // ----- 追加:ここから -----
+    /** Userを1件検索して返す */
+    public User getUser(Integer id) {
+        return userRepository.findById(id).get();
+    }
+    // ----- 追加:ここまで -----
+
     /** Userの登録を行なう */
     @Transactional
     public User saveUser(User user) {
         return userRepository.save(user);
     }
-    // ----- 追加:ここまで -----
+
 }
