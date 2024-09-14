c794a7641b44c2270ee4ee43f40b49e84ff5a1f7
marimonotamago <golupio24@gmail.com>
2024-09-15 04:09:16 +0900

    chapter11までのコミット　ログインログアウト画面

--- src/main/java/com/techacademy/entity/User.java
+++ src/main/java/com/techacademy/entity/User.java
@@ -7,14 +7,17 @@ import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne; // 追加
+import jakarta.persistence.PreRemove; // 追加
 import jakarta.persistence.Table;
-import jakarta.validation.constraints.Email; //追加
-import jakarta.validation.constraints.Max; //追加
-import jakarta.validation.constraints.Min; //追加
-import jakarta.validation.constraints.NotEmpty; //追加
-import jakarta.validation.constraints.NotNull; //追加
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 
-import org.hibernate.validator.constraints.Length; //追加
+import org.hibernate.validator.constraints.Length;
+import org.springframework.transaction.annotation.Transactional; // 追加
 
 import lombok.Data;
 
@@ -54,4 +57,19 @@ public class User {
     @Email // 追加
     @Length(max=50) // 追加
     private String email;
+
+ // ----- 追加ここから -----
+    @OneToOne(mappedBy="user")
+    private Authentication authentication;
+
+    /** レコードが削除される前に行なう処理 */
+    @PreRemove
+    @Transactional
+    private void preRemove() {
+        // 認証エンティティからuserを切り離す
+        if (authentication!=null) {
+            authentication.setUser(null);
+        }
+    }
+    // ----- 追加ここまで -----
 }
